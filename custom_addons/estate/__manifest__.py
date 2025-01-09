dfd0f83f759ec84a58319ab2d92cdd855ca5b571
Mahavir-2003 <patelmahavir2003@outlook.com>
2025-01-09 18:15:15 +0530

    Security: Add access rights for Estate module
    - Created `security/ir.model.access.csv` to define access rules
    - Granted full (read, write, create, unlink) permissions for `estate.property` model to `base.group_user`
    - Updated `__manifest__.py` to include the security file in the `data` list
    
    Commands to update:
    - Restart Odoo: `python odoo-bin -c odoo.conf -d rd-demo -u estate`

--- custom_addons/estate/__manifest__.py
+++ custom_addons/estate/__manifest__.py
@@ -2,4 +2,7 @@
 	"name": "Estate",
 	"summary": "Module for managing real estate properties and advertisements.",
 	"author": "Mahavir Patel",
+    "data": [
+        'security/ir.model.access.csv'
+	]
 }
